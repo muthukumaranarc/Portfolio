0c47b7e1b3dbd9e760fa08783fe9dc22cb877f7e
muthukumaranarc <muthukumaranarc.muthukumaranarc00@gmail.com>
2025-05-01 17:04:18 +0530

    Adding API

---
+++ API/sendMail/src/main/java/com/muthu/sendMail/MailController.java
@@ -0,0 +1,21 @@
+package com.muthu.sendMail;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+
+    @Autowired
+    private EmailService mail;
+
+    @PostMapping("/send")
+    public boolean MailSender(@RequestBody UserDetails data){
+        if(mail.sendMailToMe(data) && mail.sendMailToVsitor(data)) return true;
+        return false;
+    } 
+}
