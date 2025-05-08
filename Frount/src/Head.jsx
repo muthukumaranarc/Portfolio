bb9d681788030b4b4700ef9d46a5b663659cb567
muthukumaranarc <muthukumaranarc.muthukumaranarc00@gmail.com>
2025-05-08 16:43:46 +0530

    s

--- Frount/src/Head.jsx
+++ Frount/src/Head.jsx
@@ -23,14 +23,6 @@ function Head(){
             <img src={menu} className='menuImg'/>
         </button>
 
-        {/* {open && (
-        <div className="menu  OnlyDesktop">
-            <a href="#feedback">Feedback</a>
-            <a href="mailto:muthukumaranarc00@gmail.com">Email</a>
-            <a href="https://drive.google.com/file/d/1VNBXIq6xn_nQNVrzGxMdemkttKdXZBUb/view?usp=sharing">Resume</a>
-        </div>
-        )} */}
-
         {open && (
         <div className="menu">
             <a href="#home" className='OnlyMobile'>Home</a>
@@ -41,7 +33,7 @@ function Head(){
             <a href="#contact" className='OnlyMobile'>Contact</a>
             <a href="#feedback" className='OnlyDesktop'>Feedback</a>
             <a href="mailto:muthukumaranarc00@gmail.com" className='OnlyDesktop'>Email</a>
-            <a href="https://drive.google.com/file/d/1VNBXIq6xn_nQNVrzGxMdemkttKdXZBUb/view?usp=sharing" className='OnlyDesktop'>Resume</a>
+            <a href="https://drive.google.com/file/d/1IGsiQEtWNweQK01j0LyorBttq1U4yOlK/view?usp=sharing" className='OnlyDesktop'>Resume</a>
         </div>
         )}
         </>
--- Frount/src/Profile.jsx
+++ Frount/src/Profile.jsx
@@ -8,7 +8,7 @@ function Profile() {
         <div className='glow'>Full Stack Web Developer</div>
         <div className='quickAbout'>I’m a computer science student with 9.9CGPA. I have solid knowledge of DSA. I actively engage in tech competitions and focus on building innovation solutions that solve real-world problems effectively. </div>
         <a 
-            href='https://drive.google.com/file/d/1VNBXIq6xn_nQNVrzGxMdemkttKdXZBUb/view?usp=sharing'
+            href='https://drive.google.com/file/d/1IGsiQEtWNweQK01j0LyorBttq1U4yOlK/view?usp=sharing'
             target='_blank'
             className='CheckResume'>Check Resume</a>
         <div className='Prof_img'></div>
