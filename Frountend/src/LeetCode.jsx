d6b45a469e382d5646b749dce4ca79a9c6c0c503
muthukumaranarc <muthukumaranarc.muthukumaranarc00@gmail.com>
2025-05-08 08:28:02 +0530

    Move to git

--- Frountend/src/LeetCode.jsx
+++ Frountend/src/LeetCode.jsx
@@ -6,7 +6,7 @@ function LeetCode() {
     return (
         <div className='Leet'>
             <img src={logo} className='leet_logo'></img>
-            <p>Additionally i have Solved 40+ problems in LeetCode using Java and JavaScript  to enhance my knowledge in DSA and Core Concepts.</p>
+            <p>Additionally i have Solved 40 problems in LeetCode using Java and JavaScript  to enhance my knowledge in DSA and Core Concepts.</p>
             <img src={rank} className='leet_rank'></img>
             <a 
                 href='https://leetcode.com/u/Jq4H1BglTL/'
