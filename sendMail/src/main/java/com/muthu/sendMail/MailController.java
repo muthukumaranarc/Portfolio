ffe25d4cf752fdb8c250564f66a3045c0354141d
muthukumaranarc <muthukumaranarc.muthukumaranarc00@gmail.com>
2025-05-31 06:50:17 +0530

    Configuration files modified

--- sendMail/src/main/java/com/muthu/sendMail/MailController.java
+++ sendMail/src/main/java/com/muthu/sendMail/MailController.java
@@ -1,6 +1,5 @@
 package com.muthu.sendMail;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
